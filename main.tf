4df3c219e3f476a6b53997307633e6252766f85e
Mir Owahed Ali <133110800+mir-owahed@users.noreply.github.com>
2024-11-18 23:25:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
     backend "s3" {
-        bucket = "jhooq-terraform-s3-bucket"
+        bucket = "mir-terraform-s3-bucket"
         key    = "jhooq/terraform/remote/s3/terraform.tfstate"
         region     = "eu-central-1"
-        
+        dynamodb_table = "dynamodb-state-locking"        
     }
 }
 
