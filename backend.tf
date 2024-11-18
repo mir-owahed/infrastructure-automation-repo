5a6c5a53f3ecb8d4061f5d75db46c11895c1ea31
Mir Owahed Ali <133110800+mir-owahed@users.noreply.github.com>
2024-11-18 23:05:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = var.s3_bucket_name
-    key            = "terraform/state"
-    region         = var.aws_region
-    dynamodb_table = var.dynamodb_table_name
-  }
-}
+
