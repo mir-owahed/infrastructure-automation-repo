1088fcfacb03d99c71d7cd96759ce2dc53d471d5
Mir Owahed Ali <133110800+mir-owahed@users.noreply.github.com>
2024-11-18 23:04:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,1 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  default     = "your-region"
-}
-
-variable "s3_bucket_name" {
-  description = "S3 bucket for Terraform state"
-}
-
-variable "dynamodb_table_name" {
-  description = "DynamoDB table for state locking"
-}
 
