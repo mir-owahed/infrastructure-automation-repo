d136c014b2f29c50cb24f0f15b4ab5d1676ad57e
Mir Owahed Ali <133110800+mir-owahed@users.noreply.github.com>
2024-11-20 18:28:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
         bucket = "mir-terraform-s3-bucket"
         key    = "key/terraform.tfstate"
         region     = "ap-south-1"
-        dynamodb_table = "dynamodb-state-locking"        
+        dynamodb_table = "dynamodb-state-locking"
+        lock_timeout   = "1m"
     }
 }
