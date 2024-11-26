5d7daceee759b4e61e72f0d1159b8a536bf8ae5b
Mir Owahed Ali <133110800+mir-owahed@users.noreply.github.com>
2024-11-26 12:01:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0327f51db613d7bd2"
-  instance_type = "a1.large"
+  instance_type = "t2.micro"
 
   tags = {
     Name = var.instance_name
