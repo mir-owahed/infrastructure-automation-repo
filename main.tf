c67e46fa721b9f10bc4158b543f3dd8d715b040a
Mir Ali <bachchu333@gmail.com>
2024-11-26 11:42:26 +0530

    spot instances updated

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0aebec83a182ea7ea"
-  instance_type = "t2.micro"
+  instance_type = "a1.large"
 
   tags = {
     Name = var.instance_name
